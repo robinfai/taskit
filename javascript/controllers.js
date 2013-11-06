9d44e18c952bce5afed957e6c70708d30ad64fe8
robinfai <robinfai9@gmail.com>
2013-11-06 09:10:26 +0800

    完成列表设计

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -58,4 +58,15 @@ define(['app','semantic','directives'], function(app) {
                 $injector.invoke(HomeController, this, {'$scope': $scope});
             });
         }]);
+    app.controller('TaskAllController',['$scope', '$injector',
+        function TaskAllController($scope,$injector) {
+            require(['controllers/TaskAllController'], function(TaskAllController){
+                // injector method takes an array of modules as the first argument
+                // if you want your controller to be able to use components from
+                // any of your other modules, make sure you include it together with 'ng'
+                // Furthermore we need to pass on the $scope as it's unique to this controller
+                $injector.invoke(TaskAllController, this, {'$scope': $scope});
+            });
+        }]);
+
 });
--- javascript/controllers/TaskAllController.js
+++ javascript/controllers/TaskAllController.js
@@ -5,6 +5,20 @@ define([], function () {
     return ['$scope', '$http', '$location', function ($scope, $http, $location) {
         // You can access the scope of the controller from here
         $scope.template = {url:"partials/task/all.html"};
+        $scope.items = [
+            {
+                title:'测试任务列表',
+                content:'How does one go about reading answers to questions in a FAQ'
+            },
+            {
+                title:'测试任务列表',
+                content:'How does one go about reading answers to questions in a FAQ'
+            },
+            {
+                title:'测试任务列表',
+                content:'How does one go about reading answers to questions in a FAQ'
+            },
+        ]
         // because this has happened asynchroneusly we've missed
         // Angular's initial call to $apply after the controller has been loaded
         // hence we need to explicityly call it at the end of our Controller constructor
