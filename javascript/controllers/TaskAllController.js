d8bac0312bb34e75d913ce8281cbee1f9581478f
robinfai <robinfai@live.cn>
2013-11-19 21:51:45 +0800

    新增，看板与任务添加的效果。

--- javascript/controllers/TaskAllController.js
+++ javascript/controllers/TaskAllController.js
@@ -11,12 +11,91 @@ define([], function () {
                 content:'How does one go about reading answers to questions in a FAQ'
             })
         };
-        $scope.items = [
+        $scope.boards = [
             {
-                title:'测试任务列表',
-                content:'How does one go about reading answers to questions in a FAQ'
+                name:'测试看板',
+                items:[
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                ]
             },
-        ]
+            {
+                name:'测试看板4',
+                items:[
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                    {
+                        title:'测试任务列表',
+                        content:'How does one go about reading answers to questions in a FAQ'
+                    },
+                ]
+            }
+        ];
+        $scope.itemWidth = 350;
+        $scope.setBoardWidth = function(){
+            $scope.width = ($scope.boards.length + 1) * $scope.itemWidth + 20;
+            console.log($scope.width )
+        };
+        $scope.setBoardWidth();
+        $scope.isShowAddCard = {};
+        $scope.showAddCard = function($index){
+            for(var i in $scope.isShowAddCard){
+                $scope.isShowAddCard[i] = false;
+            }
+            $scope.isShowAddCard[$index] = true;
+            console.log($scope.isShowAddCard)
+        }
+        $scope.card = {};
+        $scope.addCard = function($index){
+            $scope.boards[$index].items.push({content:this.card[$index]})
+            this.card[$index] = '';
+            $scope.isShowAddCard[$index] = false;
+        }
+
+        $scope.isShowAddBoard = false;
+        $scope.showAddBoard = function(){
+            $scope.isShowAddBoard = true;
+        }
+
+        $scope.addBoard = function(){
+            $scope.boards.push({name:$scope.board,items:[]});
+            $scope.board = '';
+            $scope.isShowAddBoard = false;
+            $scope.setBoardWidth();
+        }
+
         // because this has happened asynchroneusly we've missed
         // Angular's initial call to $apply after the controller has been loaded
         // hence we need to explicityly call it at the end of our Controller constructor
