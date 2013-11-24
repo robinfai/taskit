b5706da2cb2336df7f321d0c038f171e10a4fa17
robinfai <robinfai9@gmail.com>
2013-11-24 22:03:47 +0800

    完成card用户添加与删除效果.

--- javascript/controllers/CardController.js
+++ javascript/controllers/CardController.js
@@ -28,10 +28,33 @@ define([], function () {
         }
 
         $scope.members = ['robinfai','taskit','jlds','ssdw','why','uudxvh','rrtchs','dsayd'];
+        $scope.selectedMember = [];
+        $scope.selectMember = function(member){
+            var index = $scope.selectedMember.indexOf(member);
+            if( index != -1){
+                console.log();
+                $scope.selectedMember.splice(index,1);
+            }else{
+                $scope.selectedMember.push(member);
+            }
+            //$scope.selectedMember[member] = !$scope.selectedMember[member];
+
+        }
 
         $scope.$on("hideAllItemOperate",function(){
             $scope.isShowItemOperate = false;
         })
+        $scope.changeColor = function($color){
+            var index = $scope.colors.indexOf($color);
+            if(index != -1){
+                $scope.colors.splice(index,1);
+            }else{
+                $scope.colors.push($color);
+            }
+
+            $scope.activeColors[$color] = !$scope.activeColors[$color];
+
+        }
 
         // because this has happened asynchroneusly we've missed
         // Angular's initial call to $apply after the controller has been loaded
